ec815ec65decccaf052de1783f384fab272465db
brennkje <kjetil_g.b@live.no>
2021-08-31 10:56:33 +0200

    Update template
    Removed some template code and added some of my own

---
+++ Shared/ItemType.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorGitHub.Shared
+{
+    public class ItemType
+    {
+        public string Name { get; }
+        public string Value { get; }
+ 
+        public ItemType(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
